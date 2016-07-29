7f3655b67aaa6c2bb01022aa79a79f14b51870a5
BenSelwynSmith <BenSelwynSmith@googlemail.com>
2016-07-30 11:31:11 +1200

    Tap and Pinch using UI Panels

---
+++ Assets/Scripts/SelectImage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectImage : MonoBehaviour {
+    
+    //bool test = true;
+    public RectTransform rt, canvasRT;
+    public GameObject highlight;
+	
+    void Start() {
+        rt = GetComponent<RectTransform>();
+        canvasRT = GameObject.Find("CanvasSlider").GetComponent<RectTransform>();
+        highlight = transform.Find("Highlight").gameObject;
+    }
+    
+    //public void Activate() { test = true; }
+    
+    public void Pinch(Vector3 pos) {
+        Vector2 screenPos = RectTransformUtility.WorldToScreenPoint(Camera.main,pos);
+        //rt.anchoredPosition = screenPos - canvasRT.sizeDelta / 2;
+        if (RectTransformUtility.RectangleContainsScreenPoint(rt, screenPos, Camera.main)) {
+            highlight.SetActive(!highlight.activeSelf);
+        }
+    }
+    
+    
+    
+    
+    
+}
